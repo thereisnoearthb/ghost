f4fb08f47442006e056820f780a989f9dc288ee8
thereisnoearthb <53037110+thereisnoearthb@users.noreply.github.com>
2020-07-24 16:03:51 +0530

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `http://https://thereisnoearthb.netlify.app`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://thereisnoearthb.netlify.app`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
